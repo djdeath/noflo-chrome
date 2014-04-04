a7b86298ebc5425c3f281418c6ab214fbad4537d
Lionel Landwerlin <llandwerlin@gmail.com>
2014-04-04 16:32:28 +0100

    finally got the runtime to talk with the UI

--- server.js
+++ server.js
@@ -1,101 +1,70 @@
 
-//var http = require('http');
-console.log(http);
-//var lhttp = http();
 var Base = require('noflo-runtime-base');
 
 function WebSocketRuntime (options) {
-  if (!options) {
-    options = {};
-  }
-  this.connections = [];
-  // if (options.catchExceptions) {
-  //   process.on('uncaughtException', function (err) {
-  //     this.connections.forEach(function (connection) {
-  //       this.send('network', 'error', {
-  //         message: err.toString()
-  //       }, {
-  //         connection: connection
-  //       });
-  //     }.bind(this));
-  //   }.bind(this));
-  // }
+    if (!options) {
+        options = {};
+    }
+    this.connections = [];
 
-  if (options.captureOutput) {
-    this.startCapture();
-  }
+    if (options.captureOutput) {
+        this.startCapture();
+    }
 
-  this.prototype.constructor.apply(this, arguments);
-  this.receive = this.prototype.receive;
+    this.prototype.constructor.apply(this, arguments);
+    this.receive = this.prototype.receive;
 }
 WebSocketRuntime.prototype = Base;
 WebSocketRuntime.prototype.send = function (protocol, topic, payload, context) {
-  if (!context.connection || !context.connection.connected) {
-    return;
-  }
-  context.connection.sendUTF(JSON.stringify({
-    protocol: protocol,
-    command: topic,
-    payload: payload
-  }));
+    if (!context.connection)
+        return;
+
+
+    context.connection.send(JSON.stringify({
+        protocol: protocol,
+        command: topic,
+        payload: payload
+    }));
 };
 
 WebSocketRuntime.prototype.startCapture = function () {
-  // this.originalStdOut = process.stdout.write;
-  // process.stdout.write = function (string, encoding, fd) {
-  //   this.connections.forEach(function (connection) {
-  //     this.send('network', 'output', {
-  //       message: string.replace(/\n$/, '')
-  //     }, {
-  //       connection: connection
-  //     });
-  //   }.bind(this));
-  // }.bind(this);
 };
 
 WebSocketRuntime.prototype.stopCapture = function () {
-  // if (!this.originalStdOut) {
-  //   return;
-  // }
-  // process.stdout.write = this.originalStdOut;
 };
 
 var runtime = function (httpServer, options) {
-  var wsServer = new http.WebSocketServer(httpServer);
+    var wsServer = new http.WebSocketServer(httpServer);
 
-  var runtime = new WebSocketRuntime(options);
-  var handleMessage = function (message, connection) {
-    if (message.type == 'utf8') {
-      try {
-        var contents = JSON.parse(message.utf8Data);
-      } catch (e) {
-        return;
-      }
-      runtime.receive(contents.protocol, contents.command, contents.payload, {
-        connection: connection
-      });
-    }
-  };
+    var runtime = new WebSocketRuntime(options);
+    var handleMessage = function (message, connection) {
+        try {
+            var contents = JSON.parse(message.data);
+        } catch (e) {
+            return;
+        }
+        runtime.receive(contents.protocol, contents.command, contents.payload, {
+            connection: connection
+        });
+    };
 
-  wsServer.addEventListener('request', function (request) {
-      console.log('getting request for : ');
-      console.log(request);
-      var connection = request.accept('noflo', request.origin);
-      runtime.connections.push(connection);
-      connection.addEventListener('message', function (message) {
-          handleMessage(message, connection);
-      });
-      connection.addEventListener('close', function () {
-          if (runtime.connections.indexOf(connection) === -1) {
-              return;
-          }
-          runtime.connections.splice(runtime.connections.indexOf(connection), 1);
-      });
+    wsServer.addEventListener('request', function (request) {
+        var connection = request.accept('noflo', request.origin);
+        runtime.connections.push(connection);
+        connection.addEventListener('message', function (message) {
+            handleMessage(message, connection);
+        });
+        connection.addEventListener('close', function () {
+            if (runtime.connections.indexOf(connection) === -1) {
+                return;
+            }
+            runtime.connections.splice(runtime.connections.indexOf(connection), 1);
+        });
 
-      return true;
-  });
+        return true;
+    });
 
-  return runtime;
+    return runtime;
 };
 
 /**/
@@ -106,13 +75,11 @@ var port = 5555;
 var interval = 10 * 60 * 1000;
 
 var server = new http.Server();
-console.log(server);
 var rt = runtime(server, {
-  baseDir: baseDir,
-  captureOutput: false,
-  catchExceptions: true
+    baseDir: baseDir,
+    captureOutput: false,
+    catchExceptions: true
 });
-console.log('runtime operating!');
 
 var NoFlo = require('noflo-chrome');
 var loader = new NoFlo.ComponentLoader('/noflo-chrome');
@@ -123,7 +90,4 @@ loader.listComponents(function(components) {
     console.log('done.');
 });
 
-server.listen(port);// , function () {
-//   console.log('NoFlo runtime listening at ws://' + host + ':' + port);
-//   console.log('Using ' + baseDir + ' for component loading');
-// });
+server.listen(port);
